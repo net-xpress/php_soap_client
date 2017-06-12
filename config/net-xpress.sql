434dbe5ed60b9388f8d9f09ba596ebcec26ce46a
Moritz Gericke <moritz@net-xpress.de>
2017-06-12 10:45:37 +0200

    added db scheme OrderHead and OrderItem

--- config/net-xpress.sql
+++ config/net-xpress.sql
@@ -25,3 +25,75 @@ CREATE TABLE IF NOT EXISTS `db473835270`.`MetaLastUpdate` (
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
   COLLATE = utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `db473835270`.`OrderHead` (
+  `Currency`                VARCHAR(45)             DEFAULT NULL,
+  `CustomerID`              INT(11)                 DEFAULT NULL,
+  `DeliveryAddressID`       INT(11)                 DEFAULT NULL,
+  `DoneTimestamp`           VARCHAR(45)             DEFAULT NULL,
+  `DunningLevel`            INT(11)                 DEFAULT NULL,
+  `EbaySellerAccount`       VARCHAR(45)             DEFAULT NULL,
+  `EstimatedTimeOfShipment` VARCHAR(45)             DEFAULT NULL,
+  `ExchangeRatio`           DECIMAL(8, 2)           DEFAULT NULL,
+  `ExternalOrderID`         VARCHAR(45)             DEFAULT NULL,
+  `Invoice`                 VARCHAR(45)             DEFAULT NULL,
+  `IsNetto`                 TINYINT(1)              DEFAULT NULL,
+  `LastUpdate`              INT(11)                 DEFAULT NULL,
+  `Marking1ID`              INT(11)                 DEFAULT NULL,
+  `MethodOfPaymentID`       INT(11)                 DEFAULT NULL,
+  `StoreID`                 INT(11)                 DEFAULT NULL,
+  `OrderDocumentNumbers`    INT(11)                 DEFAULT NULL,
+  `OrderID`                 INT(11) NOT NULL        DEFAULT '0',
+  `OrderInfos`              INT(11)                 DEFAULT NULL,
+  `OrderStatus`             DECIMAL(8, 2)           DEFAULT NULL,
+  `OrderTimestamp`          INT(11)                 DEFAULT NULL,
+  `OrderType`               VARCHAR(45)             DEFAULT NULL,
+  `PackageNumber`           VARCHAR(45)             DEFAULT NULL,
+  `PaidTimestamp`           VARCHAR(45)             DEFAULT NULL,
+  `ParentOrderID`           INT(11)                 DEFAULT NULL,
+  `PaymentStatus`           INT(11)                 DEFAULT NULL,
+  `ReferrerID`              DECIMAL(8, 2)           DEFAULT NULL,
+  `RemoteIP`                VARCHAR(45)             DEFAULT NULL,
+  `ResponsibleID`           INT(11)                 DEFAULT NULL,
+  `SalesAgentID`            INT(11)                 DEFAULT NULL,
+  `SellerAccount`           VARCHAR(45)             DEFAULT NULL,
+  `ShippingCosts`           DECIMAL(8, 2)           DEFAULT NULL,
+  `ShippingID`              INT(11)                 DEFAULT NULL,
+  `ShippingMethodID`        INT(11)                 DEFAULT NULL,
+  `ShippingProfileID`       INT(11)                 DEFAULT NULL,
+  `TotalBrutto`             DECIMAL(10, 4)          DEFAULT NULL,
+  `TotalInvoice`            DECIMAL(10, 4)          DEFAULT NULL,
+  `TotalNetto`              DECIMAL(10, 4)          DEFAULT NULL,
+  `TotalVAT`                DECIMAL(8, 2)           DEFAULT NULL,
+  `WarehouseID`             INT(11)                 DEFAULT NULL,
+  PRIMARY KEY (`OrderID`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `db473835270`.`OrderItem` (
+  `id`                   INT(11) NOT NULL        AUTO_INCREMENT,
+  `BundleItemID`         INT(11)                 DEFAULT NULL,
+  `Currency`             VARCHAR(45)             DEFAULT NULL,
+  `ExternalItemID`       VARCHAR(45)             DEFAULT NULL,
+  `ExternalOrderItemID`  VARCHAR(45)             DEFAULT NULL,
+  `ItemID`               INT(11)                 DEFAULT NULL,
+  `ItemNo`               VARCHAR(45)             DEFAULT NULL,
+  `ItemRebate`           DECIMAL(8, 2)           DEFAULT NULL,
+  `ItemText`             VARCHAR(45)             DEFAULT NULL,
+  `NeckermannItemNo`     VARCHAR(45)             DEFAULT NULL,
+  `OrderID`              INT(11)                 DEFAULT NULL,
+  `OrderRowID`           INT(11)                 DEFAULT NULL,
+  `Price`                DECIMAL(10, 4)          DEFAULT NULL,
+  `Quantity`             DECIMAL(8, 2)           DEFAULT NULL,
+  `ReferrerID`           DECIMAL(8, 2)           DEFAULT NULL,
+  `SKU`                  VARCHAR(45)             DEFAULT NULL,
+  `SalesOrderProperties` INT(11)                 DEFAULT NULL,
+  `VAT`                  DECIMAL(8, 2)           DEFAULT NULL,
+  `WarehouseID`          INT(11)                 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
