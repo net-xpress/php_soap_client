bbf3ecada5e8fc13a11d83e224f93573225b219f
Moritz Gericke <moritz@net-xpress.de>
2017-06-20 12:52:20 +0200

    updated script added verbose comments

--- scripts/ExecuteOncePerDay.php
+++ scripts/ExecuteOncePerDay.php
@@ -3,11 +3,38 @@ require_once realpath( dirname( __FILE__ ).'/../' ).'/config/basic.inc.php';
 require_once ROOT.'lib/soap/example_loader/PlentymarketsSoapExampleLoader.class.php';
 require_once ROOT.'scripts/calculation/CalculateDailyNeed.class.php';
 
-// update order databases: OrderHead and OrderItem
+// via plenty soap,
+// update:	OrderHead, OrderItem
+// rely:	MetaLastUpdate, plenty_soap_token
 PlentymarketsSoapExampleLoader::getInstance()->run( ['', 'SearchOrders',] );
 
-// update item databases: ItemsBase, ItemAvailability, ItemCategories, ItemFreeTextFields, ItemTexts, ItemOthers, ItemStock
+// via plenty soap,
+// update:	ItemsBase, ItemAvailability, ItemCategories, ItemAttributeValueSets, ItemFreeTextFields, ItemTexts, ItemOthers, ItemStock
+// rely:	MetaLastUpdate, plenty_soap_token
 PlentymarketsSoapExampleLoader::getInstance()->run( ['', 'GetItemsBase',] );
 
-// calculate daily need
+// locally, calculate daily need,
+// update:	CalculatedDailyNeed
+// rely:	OrderHead, ItemsBase, ItemFreeTextFields, ItemAvailability
 (new CalculateDailyNeed())->execute();
+
+// via plenty soap,
+// update:	ItemsWarehouseSettings
+// rely:	ItemsBase, ItemAttributeValueSets, plenty_soap_token
+PlentymarketsSoapExampleLoader::getInstance()->run( ['', 'GetItemsWarehouseSettings',] );
+
+// via plenty soap,
+// update:	ItemsSuppliers
+// rely:	ItemsBase, plenty_soap_token
+PlentymarketsSoapExampleLoader::getInstance()->run( ['', 'GetItemsSuppliers',] );
+
+// via plenty soap,
+// update:	WarehouseList
+// rely:	plenty_soap_token
+PlentymarketsSoapExampleLoader::getInstance()->run( ['', 'GetWarehouseList',] );
+
+// via plenty soap:
+// update:	CurrentStocks
+// rely:	WarehouseList, MetaLastUpdate, plenty_soap_token
+PlentymarketsSoapExampleLoader::getInstance()->run( ['', 'GetCurrentStocks',] );
+
