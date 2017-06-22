167a5214b1de936aea49af9167fdf2c39ad77111
Moritz Gericke <moritz@net-xpress.de>
2017-06-22 13:38:59 +0200

    added main import script

---
+++ scripts/JansenStockUpdate.php
@@ -0,0 +1,15 @@
+<?php
+require_once realpath( dirname( __FILE__ ).'/../' ).'/config/basic.inc.php';
+require_once ROOT.'lib/soap/example_loader/PlentymarketsSoapExampleLoader.class.php';
+require_once ROOT.'scripts/import/ImportJansenStock.class.php';
+
+/**
+ * This script performs the 'jansen stock difference update job': Read stock data from csv-file, match jansen articles
+ * against net-xpress articles, write differences back to plenty. Remark: Just writing differences prevents updating all
+ * matched articles
+ */
+
+// locally, read and evaluate csv file,
+// update:	JansenTransactionHead, JansenTransactionItem, JansenStockData
+// rely:	MetaConfig
+(new ImportJansenStock( '/kunden/homepages/22/d66025481/htdocs/stock_jd/stock.csv' ))->execute();
